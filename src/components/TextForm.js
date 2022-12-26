a1e7749d4fff3ba7318383640040ac976cf43815
Mohnish <mohnish.a007@gmail.com>
2022-12-26 03:02:07 +0000

    More feature added using react hook

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,31 +1,50 @@
-import React, {useState} from 'react'
-
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    const clickHandler = () => {
-        //console.log("Upper clicked" + text) // We can access the text value here and originally the react hook
-        let newtext = text.toUpperCase(); // text variable is coming from the values of react hook
-        setText(newtext);
-    }
+  const clickHandler = () => {
+    //console.log("Upper clicked" + text) // We can access the text value here and originally the react hook
+    let newtext = text.toUpperCase(); // text variable is coming from the values of react hook
+    setText(newtext);
+  };
 
-    const changeHandler = (event) => {
-        //console.log("change clicked")
-        setText(event.target.value)
-    }
-    const [text, setText] = useState("Enter Your text here");
-   // text = "new State"; // WRONG WAY TO CHANGE THE STATE
-   // setText("New changes in text"); //This is the correct way
-   
+  const changeHandler = (event) => {
+    //console.log("change clicked")
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter Your text here");
+  // text = "new State"; // WRONG WAY TO CHANGE THE STATE
+  // setText("New changes in text"); //This is the correct way
+
   return (
     <>
-    <h1>{props.headings}</h1>
-    <form>
-  <div className="form-group">
-    <textarea type="text" className="form-control" id="mybox" onChange={changeHandler} value={text} placeholder="Type your text Here"/>
-  </div>
-  <br/>
-   <button type='button' className="btn btn-primary" onClick={clickHandler}>Convert to Upper Case</button>
-</form>
+      <h1>{props.headings}</h1>
+      <div className="container">
+      <form>
+        <div className="form-group">
+          <textarea
+            type="text"
+            className="form-control"
+            id="mybox"
+            onChange={changeHandler}
+            value={text}
+            placeholder="Type your text Here"
+          />
+        </div>
+        <br />
+        <button
+          type="button"
+          className="btn btn-primary"
+          onClick={clickHandler}
+        >
+          Convert to Upper Case
+        </button>
+      </form>
+      </div>
+      <br/>
+      <div className="container my-3">
+        <h3>Text Summary</h3>
+        <p>{text.split(" ").length} words, {text.length} Characters</p>
+      </div>
     </>
-  )
+  );
 }
