4034aeabcf6e40a73dfb2662839ade08b406e0fb
Mohnish <mohnish.a007@gmail.com>
2023-01-17 20:41:19 +0000

    The perfect dark and light mode scheme

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar'; //Always have components start with Capital letters
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('dark');
+  const toggleMode = () => {
+    if (mode === 'light'){
+      setMode('dark')
+    } else {
+      setMode('light')
+    }
+  }
   return (
     <>
    
     <div className="container my-3">
-     <Navbar title="Text Utility"/>
+     <Navbar title="Text Utility" mode={mode} toggleMode={toggleMode}/>
      <TextForm headings="Text Area"/>
-      <About/>
+      {/* <About/> */}
       {/* <Navbar/> */}
     </div>
     
